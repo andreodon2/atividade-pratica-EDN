0d20eb0e368292793f69f9a8cb7583e25f885d41
André Freitas <andreodon@gmail.com>
2025-06-24 19:58:33 -0300

    Conclusão do exercício calculadora de soma

---
+++ calculadora-de-soma.py
@@ -0,0 +1,9 @@
+# Calculadora de Soma
+# Desenvolva um programa que soma dois números. Use as variáveis numero1 = 12 e numero2 = 14. O programa deve calcular a soma e exibir o resultado
+
+numero1 = int(input('Digite o primeiro número 1: '))
+numero2 = int(input('Digite o número 2: '))
+
+soma = numero1 + numero2
+
+print(f'A soma entre o número 1: {numero1} e número 2: {numero2} é igual a {soma}')
